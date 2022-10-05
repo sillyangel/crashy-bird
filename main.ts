5f26a7561d96ecb2c974e719ce05a794607ee9de
sillyangel <sillyangel3@gmail.com>
2022-10-05 07:32:36 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,52 @@
+gamerbit.onEvent(GamerBitPin.P12, GamerBitEvent.Down, function () {
+	
+})
+input.onButtonPressed(Button.A, function () {
+	
+})
+gamerbit.onEvent(GamerBitPin.P16, GamerBitEvent.Down, function () {
+	
+})
+gamerbit.onEvent(GamerBitPin.P1, GamerBitEvent.Down, function () {
+    bird.change(LedSpriteProperty.X, -1)
+})
+gamerbit.onEvent(GamerBitPin.P8, GamerBitEvent.Down, function () {
+    bird.change(LedSpriteProperty.Y, 1)
+})
+gamerbit.onEvent(GamerBitPin.P2, GamerBitEvent.Down, function () {
+    bird.change(LedSpriteProperty.X, 1)
+})
+gamerbit.onEvent(GamerBitPin.P0, GamerBitEvent.Down, function () {
+    bird.change(LedSpriteProperty.Y, -1)
+})
+let emptyObstacleY = 0
+let ticks = 0
+let bird: game.LedSprite = null
+let index = 0
+let obstacles: game.LedSprite[] = []
+bird = game.createSprite(0, 2)
+bird.set(LedSpriteProperty.Blink, 300)
+basic.forever(function () {
+    while (obstacles.length > 0 && obstacles[0].get(LedSpriteProperty.X) == 0) {
+        obstacles.removeAt(0).delete()
+    }
+    for (let obstacle2 of obstacles) {
+        obstacle2.change(LedSpriteProperty.X, -1)
+    }
+    if (ticks % 3 == 0) {
+        game.addScore(1)
+        emptyObstacleY = randint(0, 4)
+        for (let index2 = 0; index2 <= 4; index2++) {
+            if (index2 != emptyObstacleY) {
+                obstacles.push(game.createSprite(4, index2))
+            }
+        }
+    }
+    for (let obstacle3 of obstacles) {
+        if (obstacle3.get(LedSpriteProperty.X) == bird.get(LedSpriteProperty.X) && obstacle3.get(LedSpriteProperty.Y) == bird.get(LedSpriteProperty.Y)) {
+            game.gameOver()
+        }
+    }
+    ticks += 1
+    basic.pause(1000)
+})
